f20678f0bcefa9a451b481add22ff38a8ff860aa
Gerry Waterston <gwaterston@hotmail.co.uk>
2019-03-05 15:18:21 +0000

    Set up PokemonContainer

---
+++ src/containers/PokemonContainer.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+class PokemonContainer extends React.Component {
+  constructor(props){
+      super(props);
+      this.state = {
+          pokemons: [],
+          currentPokemon: null
+      }
+// bind goes here
+  }
+
+componentDidMount() {
+    const url = "https://pokeapi.co/api/v2/pokemon/?limit=151";
+    fetch(url)
+    .then(res => res.json())
+    .then(data => this.setState({pokemons: data}))
+}
+
+
+
+
+  
+    render(){
+        return (
+          <div>
+              <h2>Pokemon Container</h2>
+
+
+          </div>
+        )
+    }
+}
+
+export default PokemonContainer;
