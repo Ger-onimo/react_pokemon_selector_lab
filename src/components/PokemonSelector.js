dc89c1a4a8d2c8dfaab569f8e027ca25be523816
Gerry Waterston <gwaterston@hotmail.co.uk>
2019-03-05 15:58:06 +0000

    Set up PokemonSelector handle change to populate drop down

--- src/components/PokemonSelector.js
+++ src/components/PokemonSelector.js
@@ -3,7 +3,20 @@ import React from "react";
 const PokemonSelector = (props) => {
     const options = props.pokemons.map((pokemon, index) => {
       return <option value={index} key={index}>
-      pokemon.name</option>
+      {pokemon.name}</option>
     })
-    
+
+    function handleChange(event){
+        props.onPokemonSelected(event.target.value);
+    }
+
+
+    return(
+        <select id="pokemon-selector" onChange={handleChange} defaultValue="default">
+          <option disabled value="default">Choose a Pokemon...</option>
+          {options}
+        </select>
+    )
 }
+
+export default PokemonSelector;
--- src/containers/PokemonContainer.js
+++ src/containers/PokemonContainer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PokemonSelector from "../components/PokemonSelector";
 
 class PokemonContainer extends React.Component {
   constructor(props){
@@ -6,27 +7,28 @@ class PokemonContainer extends React.Component {
       this.state = {
           pokemons: [],
           currentPokemon: null
-      }
-// bind goes here
+      };
+      this.handlePokemonSelected = this.handlePokemonSelected.bind(this);
   }
 
 componentDidMount() {
     const url = "https://pokeapi.co/api/v2/pokemon/?limit=151";
     fetch(url)
     .then(res => res.json())
-    .then(data => this.setState({pokemons: data}))
+    .then(data => this.setState({pokemons: data.results}))
 }
 
+handlePokemonSelected(index){
+    const selectedPokemon = this.state.pokemons[index];
+    this.setState({currentPokemon: selectedPokemon})
+}
 
-
-
-  
     render(){
         return (
           <div>
             <h2>Pokemon Container</h2>
             <PokemonSelector pokemons={this.state.pokemons}/>
-            <PokemonDetail/>
+            {/* <PokemonDetail/> */}
           </div>
         )
     }
